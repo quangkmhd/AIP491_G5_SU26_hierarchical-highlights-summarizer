6540dba87fe4681e51e5f0c32ee81a7c72c55caa
quangkmhd <quangkmhd09344@gmail.com>
2026-08-18 22:44:07 +0700

    refactor(submission): remove unused errors.py and audio_diagnostics.py files

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -4,7 +4,6 @@ from ._base import ConfigBase
 from .abstractive import AbstractiveConfig
 from .asr import AsrConfig
 from .chunking import ChunkingConfig
-from .errors import ConfigError
 from .language import LanguageConfig
 from .recap import MeetingRecapConfig
 from .sliding_text_tiling import SlidingTextTilingConfig
@@ -12,7 +11,6 @@ from .sliding_text_tiling import SlidingTextTilingConfig
 __all__ = [
     "AsrConfig",
     "ConfigBase",
-    "ConfigError",
     "SlidingTextTilingConfig",
     "ChunkingConfig",
     "AbstractiveConfig",
