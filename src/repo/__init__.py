379b90d6b691dcada76373446b7725a5492811b3
quangkmhd <quangkmhd09344@gmail.com>
2026-08-18 22:47:27 +0700

    refactor(submission): remove unused repo files transcript_repo, recap_repo, _io, prompts_vi

--- src/repo/__init__.py
+++ src/repo/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-from ._io import RepoIOError, read_json_file, write_json_file
 from .model_loader import (
-    CHUNK_SUMMARIZER_PATH, TOPIC_TITLER_PATH, ModelHandle, ModelKind,
-    ModelLoadError, ModelLoader,
+    CHUNK_SUMMARIZER_PATH,
+    TOPIC_TITLER_PATH,
+    ModelHandle,
+    ModelKind,
+    ModelLoadError,
+    ModelLoader,
 )
-from .recap_repo import RecapRepo, RecapRepoError
-from .prompts_vi import SUMMARY_PREFIX_VI, TITLE_PREFIX_VI
 from .seq2seq_inference import (
     BARTphoTopicTitler,
     ChunkSummarizer,
@@ -14,7 +15,6 @@ from .seq2seq_inference import (
     TopicTitler,
     ViT5ChunkSummarizer,
 )
-from .transcript_repo import TranscriptRepo, TranscriptRepoError
 
 __all__ = [
     "ModelLoader",
@@ -28,13 +28,4 @@ __all__ = [
     "ViT5ChunkSummarizer",
     "BARTphoTopicTitler",
     "GenerationError",
-    "SUMMARY_PREFIX_VI",
-    "TITLE_PREFIX_VI",
-    "TranscriptRepo",
-    "TranscriptRepoError",
-    "RecapRepo",
-    "RecapRepoError",
-    "RepoIOError",
-    "read_json_file",
-    "write_json_file",
 ]
--- src/repo/prompts_vi.py
+++
@@ -1,4 +0,0 @@
-SUMMARY_PREFIX_VI = "Tóm tắt: "
-TITLE_PREFIX_VI = "Tạo tiêu đề: "
-
-__all__ = ["SUMMARY_PREFIX_VI", "TITLE_PREFIX_VI"]
