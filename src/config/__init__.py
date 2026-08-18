9a0df7419c0502af5808edd7f3e63b31c2c3edb2
quangkmhd <quangkmhd09344@gmail.com>
2026-08-18 22:53:48 +0700

    refactor(submission): remove unused recap.py master config file

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,12 +2,10 @@ from __future__ import annotations
 
 from ._base import ConfigBase
 from .asr import AsrConfig
-from .recap import MeetingRecapConfig
 from .sliding_text_tiling import SlidingTextTilingConfig
 
 __all__ = [
     "AsrConfig",
     "ConfigBase",
     "SlidingTextTilingConfig",
-    "MeetingRecapConfig",
 ]
